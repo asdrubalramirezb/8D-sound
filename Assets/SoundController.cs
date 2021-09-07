4aafee3f9c49922e956dddcbf9ed65b3aecb30e5
asdrubalramirezb <46140948+asdrubalramirezb@users.noreply.github.com>
2021-09-06 22:44:11 -0500

    Initial commit

---
+++ Assets/SoundController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip sounds;
+    public GameObject GOCubo; 
+       
+    
+    public void reproducirClips (){
+      AudioSource sonido = GOCubo.GetComponent<AudioSource>();
+      sonido.clip = sounds;
+      sonido.Play ();
+    }
+}
---
+++ Assets/qundiocontroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class qundiocontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cubo; 
+    public AudioClip audioclip;
+
+    public void activarsonido () {
+     
+     AudioSource sonido = cubo.GetComponent<AudioSource> ();
+     sonido.clip = audioclip;
+     sonido.Play ();
+    }
+}
